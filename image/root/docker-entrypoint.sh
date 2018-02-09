e99f470cd5bcb49e679bfd7bb6f45b6b21772211
Ioanna M Dimitriou H <ioanna.m.dimitriou@gmail.com>
2018-02-09 08:46:32 +0100

    Now container will always be started with Zookeeper and Kafka servers on

--- image/root/docker-entrypoint.sh
+++ image/root/docker-entrypoint.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-set -x
+# set -x # uncomment for debugging
 cd /opt/kafka
 su - kafka
 
+# Start every container with Zookeeper and Kafka running detached. 
 
-# Add kafka as command if needed
-
-# if no arguments are given to the container, or if the only argument is "kafka"
-if   [[ -z "$1" || ( "$1" = 'kafka' && -z "$2" ) ]] 
-# then start kafka with the default config file I made
-then 
-echo "Starting zookeeper server"
-# run it detached without using disown or nohup
+echo "Starting Zookeeper server"
 ((bin/zookeeper-server-start.sh config/zookeeper.properties)&)&
 echo "Starting kafka server"
-bin/kafka-server-start.sh config/server.properties
-# else just execute the argument that was given at the start of the container.
-else exec $*
-fi
+((bin/kafka-server-start.sh config/server.properties)&)&
+
+
