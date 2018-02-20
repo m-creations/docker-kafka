a67afe0c319778e9a6af0c2dd0fad105c05a6d9a
Ioanna M Dimitriou H <ioanna.m.dimitriou@gmail.com>
2018-02-20 09:19:46 +0100

    this should now really fix the hanging test

--- tests/test-multiple-brokers.sh
+++ tests/test-multiple-brokers.sh
@@ -8,5 +8,5 @@ docker run -d --name multiple -v "${LOCAL_DIR}/tests/internal/multiple-broker.sh
 
 sleep 5
 
-docker exec multiple sh /opt/kafka/multiple-broker.sh
+docker exec -it multiple sh /opt/kafka/multiple-broker.sh
 
